d0d98504fb1eeae2cb5d7f22430cb360d7cbf4d4
kamun <kamuntu.collins@gmail.com>
2019-10-27 14:03:52 +0300

    fourth commit ABOUT BROADCAST RECEIVERS AND INTENTS

---
+++ app/src/main/java/com/example/fcm/Broad_cast.java
@@ -0,0 +1,18 @@
+package com.example.fcm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class Broad_cast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+            Toast.makeText(context, "boot completed successfully", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+}
